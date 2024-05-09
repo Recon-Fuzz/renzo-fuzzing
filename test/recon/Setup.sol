68c87fdd0b3e251eb9063f05cedcb550d4bb9f98
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-05-09 14:09:46 -0300

    chore: EigenLayer fork integration

--- test/recon/Setup.sol
+++ test/recon/Setup.sol
@@ -6,7 +6,8 @@ import { RenzoSetup } from "test/RenzoSetup.sol";
 
 abstract contract Setup is RenzoSetup, BaseSetup {
     function setup() internal virtual override {
-        // calling RenzoSetup::deployRenzo to deploy the renzo system, this includes deploying a local version of EigenLayer
-        deployRenzo();
+        // NOTE: this deploys the renzo system,
+        // this includes deploying a local version of EigenLayer
+        deployRenzo(true);
     }
 }
