480865213e98b1bcfeb57dffbb670c011dd435c4
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-05-15 13:20:57 -0300

    test: target functions to provide remaining coverage in RestakeManager

--- test/recon/RestakeManagerTargets.sol
+++ test/recon/RestakeManagerTargets.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import { BaseTargetFunctions } from "@chimera/BaseTargetFunctions.sol";
 import { BeforeAfter } from "./BeforeAfter.sol";
 import { Properties } from "./Properties.sol";
+import { IOperatorDelegator } from "../../contracts/Delegation/IOperatorDelegator.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { vm } from "@chimera/Hevm.sol";
 
@@ -16,6 +17,7 @@ abstract contract RestakeManagerTargets is BaseTargetFunctions, Properties, Befo
         restakeManager.deposit(collateralToken, amount);
     }
 
+    // NOTE: allowing this to use fully random referralId for now, could test depositing for invalid referrals with a properly defined property
     function restakeManager_depositReferral(
         uint256 tokenIndex,
         uint256 amount,
@@ -47,11 +49,56 @@ abstract contract RestakeManagerTargets is BaseTargetFunctions, Properties, Befo
         restakeManager.setTokenTvlLimit(IERC20(tokenToLimit), amount);
     }
 
-    // NOTE: danger, this allows the fuzzer to fill the buffer but may have unintended side-effects in withdrawals
-    // function restakeManager_fillBuffer() public {
+    // NOTE: this is a privileged function that's called by the DepositQueue to sweep ERC20 rewards tokens into RestakeManager
+    function restakeManager_depositTokenRewardsFromProtocol(uint256 tokenIndex) public {
+        address tokenToDeposit = _getRandomDepositableToken(tokenIndex);
+
+        // the call in depositQueue makes a call to depositTokenRewardsFromProtocol
+        depositQueue.sweepERC20(IERC20(tokenToDeposit));
+    }
+
+    // NOTE: this needs to be included to complete the native ETH staking process
+    // @audit currently ETH deposit contract is being mocked by ETHPOSDepositMock so signature values are irrelevant
+    function restakeManager_stakeEthFromQueue(
+        uint256 operatorDelegatorIndex,
+        bytes calldata pubkey,
+        bytes calldata signature,
+        bytes32 depositDataRoot
+    ) public {
+        IOperatorDelegator operatorDelegator = _getRandomOperatorDelegator(operatorDelegatorIndex);
+
+        depositQueue.stakeEthFromQueue(operatorDelegator, pubkey, signature, depositDataRoot);
+    }
+
+    // NOTE: danger, this allows the fuzzer to fill the buffer but may have unintended side-effects for overall system behavior
+    function restakeManager_fillBuffer(uint256 collateralTokenIndex) public {
+        address collateralToken = _getRandomDepositableToken(collateralTokenIndex);
+
+        uint256 bufferToFill = depositQueue.withdrawQueue().getBufferDeficit(
+            address(collateralToken)
+        );
+
+        // the target contract gets minted both of the collateral tokens in setup
+        IERC20(collateralToken).transfer(address(depositQueue.withdrawQueue()), bufferToFill);
+    }
+
+    // function restakeManager_emptyBuffer() public {
+
     // }
 
     function _getRandomDepositableToken(uint256 tokenIndex) internal view returns (address) {
         return lstAddresses[tokenIndex % lstAddresses.length];
+    }
+
+    function _getRandomOperatorDelegator(
+        uint256 operatorDelegatorIndex
+    ) internal view returns (IOperatorDelegator operatorDelegator) {
+        IOperatorDelegator[] memory operatorDelegatorArray = new IOperatorDelegator[](
+            restakeManager.getOperatorDelegatorsLength()
+        );
+        operatorDelegatorArray[0] = operatorDelegator1;
+        operatorDelegatorArray[1] = operatorDelegator2;
+
+        return operatorDelegatorArray[operatorDelegatorIndex % operatorDelegatorArray.length];
     }
 }
