cca96cd870040b7ad2c521f6776e6aca80ec6a6a
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-05-20 11:04:43 -0300

    chore: adding RestakeManager admin functions to separate target

--- test/recon/CryticTester.sol
+++ test/recon/CryticTester.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.0;
 
 import { RestakeManagerTargets } from "./RestakeManagerTargets.sol";
+import { RestakeManagerAdminTargets } from "./RestakeManagerAdminTargets.sol";
 import { CryticAsserts } from "@chimera/CryticAsserts.sol";
 
 // echidna . --contract CryticTester --config echidna.yaml
 // medusa fuzz
-contract CryticTester is RestakeManagerTargets, CryticAsserts {
+contract CryticTester is RestakeManagerTargets, RestakeManagerAdminTargets, CryticAsserts {
     constructor() payable {
         setup();
     }
